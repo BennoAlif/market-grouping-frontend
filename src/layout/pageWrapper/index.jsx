b54d99086c97c1da1a4c0e8047475cd3bbcb9ba6
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-08-12 20:11:39 +0700

    fix pageWrapper

--- src/layout/pageWrapper/index.jsx
+++ src/layout/pageWrapper/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
+import { sPageWrapper } from './styles'
+
 const PageWrapper = ({ children }) => {
     return (
         <>
             <Helmet>
                 <title>Market Grouping</title>
             </Helmet>
-            <div>{children}</div>
+            <div className={sPageWrapper}>{children}</div>
         </>
     )
 }
