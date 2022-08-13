e861fa69acf3c1df652be52005529d96d714ae86
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-08-13 23:11:25 +0700

    fix header

---
+++ src/layout/header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Wrapper } from '@/components'
+
+import { sHeader } from './styles'
+
+const Header = () => {
+    return (
+        <Wrapper>
+            <div className={sHeader}>
+                <p>Segmentasi Pasar</p>
+            </div>
+        </Wrapper>
+    )
+}
+
+export default Header
---
+++ src/layout/header/styles.js
@@ -0,0 +1,14 @@
+import { css } from '@emotion/css'
+
+export const sHeader = css`
+    width: 100%;
+
+    text-align: center;
+
+    padding: 40px 0;
+
+    & p {
+        font-size: 32px;
+        font-weight: bold;
+    }
+`
