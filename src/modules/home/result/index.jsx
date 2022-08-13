5007e86b37e723a4fafa58890ba782543f643bd2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-08-13 23:11:49 +0700

    fix home result

---
+++ src/modules/home/result/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Result = () => {
+    return (
+        <div>
+            <p>Silakan untuk memilih minat terlebih dahulu.</p>
+        </div>
+    )
+}
+
+export default Result
