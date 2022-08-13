4fa2c88d04f2ee5ba1616585cbd2237f884f7f08
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-08-14 00:25:52 +0700

    fix home result

--- src/modules/home/result/index.jsx
+++ src/modules/home/result/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const Result = () => {
+import Item from './item'
+
+import { sResult, sResultContent } from './styles'
+
+const Result = ({ subgraph, isInitial, isLoading }) => {
     return (
-        <div>
-            <p>Silakan untuk memilih minat terlebih dahulu.</p>
+        <div className={sResult}>
+            {isInitial ? (
+                <div>
+                    <p>Silakan untuk memilih minat terlebih dahulu.</p>
+                </div>
+            ) : isLoading ? (
+                <div>
+                    <p>Loading...</p>
+                </div>
+            ) : (
+                <div className={sResultContent}>
+                    {Object.values(subgraph).map((item, index) => {
+                        return (
+                            <Item
+                                key={index}
+                                accountList={item.links}
+                                title={`Segment ${index + 1} - ${
+                                    item.links.length
+                                } Akun`}
+                            />
+                        )
+                    })}
+                </div>
+            )}
         </div>
     )
 }
---
+++ src/modules/home/result/styles.js
@@ -0,0 +1,13 @@
+import { css } from '@emotion/css'
+
+export const sResult = css`
+    width: 100%;
+`
+
+export const sResultContent = css`
+    width: 100%;
+
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+`
