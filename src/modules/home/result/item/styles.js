691526a8495b5513240caeff04245a2cd01304c9
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-08-14 00:26:00 +0700

    fix home result item

---
+++ src/modules/home/result/item/styles.js
@@ -0,0 +1,22 @@
+import { css } from '@emotion/css'
+
+export const sItem = css`
+    width: 100%;
+
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+`
+
+export const sItemContent = css`
+    width: 100%;
+
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+`
+
+export const sItemTitle = css`
+    font-size: 16px;
+    font-weight: bold;
+`
