c51691fcb33947ec2983aff6b6eac9f311b2e039
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-08-14 11:06:09 +0700

    set home result

--- src/modules/home/result/index.jsx
+++ src/modules/home/result/index.jsx
@@ -1,79 +1,57 @@
-import React, { useRef, useState } from 'react';
+import ForceGraph2D from 'react-force-graph-2d'
+import React, { useRef, useEffect, useState } from 'react'
 
-import Item from './item';
-import ForceGraph2D from 'react-force-graph-2d';
-import { sResult, sResultContent } from './styles';
-const data = {
-  nodes: [
-    { id: 'A' },
-    { id: 'B' },
-    { id: 'C' },
-    { id: 'D' },
-    { id: 'E' },
-    { id: 'F' },
-  ],
-  links: [
-    { source: 'A', target: 'B', value: 5 },
-    { source: 'B', target: 'C', value: 5 },
-    { source: 'C', target: 'D', value: 5 },
-    { source: 'D', target: 'E', value: 5 },
-    { source: 'E', target: 'F', value: 5 },
-    { source: 'C', target: 'F', value: 5 },
-  ],
-};
-const Result = ({ subgraph, isInitial, isLoading }) => {
-  const [force, setForce] = useState({
-    nodes: [
-      { id: 'A' },
-      { id: 'B' },
-      { id: 'C' },
-      { id: 'D' },
-      { id: 'E' },
-      { id: 'F' },
-    ],
-    links: [
-      { source: 'A', target: 'B', value: 5 },
-      { source: 'B', target: 'C', value: 5 },
-      { source: 'C', target: 'D', value: 5 },
-      { source: 'D', target: 'E', value: 5 },
-      { source: 'E', target: 'F', value: 5 },
-      { source: 'C', target: 'F', value: 5 },
-    ],
-  });
-  const forceRef = useRef(null);
-  return (
-    <div className={sResult}>
-      <ForceGraph2D
-        graphData={force}
-        width={690}
-        height={300}
-        // backgroundColor="aliceblue"
-        nodeLabel="id"
-        ref={forceRef}
-      />
-      {isInitial ? (
-        <div>
-          <p>Silakan untuk memilih minat terlebih dahulu</p>
-        </div>
-      ) : isLoading ? (
-        <div>
-          <p>Loading...</p>
-        </div>
-      ) : (
-        <div className={sResultContent}>
-          {subgraph.map((item) => {
-            return (
-              <Item
-                key={item.id}
-                accountList={item.links}
-                title={`Subgraph ${item.id} - ${item.nodes.length} Akun`}
-              />
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
-};
+import Item from './item'
+import { sResult, sResultGraph, sResultInitial, sResultContent } from './styles'
 
-export default Result;
+const Result = ({ graph, setGraph, subgraph, isInitial, isLoading }) => {
+    const forceRef = useRef(null)
+
+    const [force, setForce] = useState()
+
+    useEffect(() => {
+        if (graph.nodes) {
+            setForce({ ...graph })
+        }
+    }, [graph])
+
+    return (
+        <div className={sResult}>
+            <div className={sResultGraph}>
+                <ForceGraph2D
+                    graphData={force}
+                    width={690}
+                    height={300}
+                    backgroundColor='aliceblue'
+                    nodeLabel='id'
+                    ref={forceRef}
+                />
+            </div>
+            {isInitial ? (
+                <div className={sResultInitial}>
+                    <p>Silakan untuk memilih minat terlebih dahulu</p>
+                </div>
+            ) : isLoading ? (
+                <div className={sResultInitial}>
+                    <p>Loading...</p>
+                </div>
+            ) : (
+                <div className={sResultContent}>
+                    {subgraph.map((item) => {
+                        return (
+                            <Item
+                                data={item}
+                                key={item.id}
+                                setGraph={setGraph}
+                                accountList={item.links}
+                                title={`Subgraph ${item.id} - ${item.nodes.length} Akun`}
+                            />
+                        )
+                    })}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Result
